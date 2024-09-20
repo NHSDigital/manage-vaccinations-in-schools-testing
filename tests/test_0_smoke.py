8ea7f4546c98885e35da40cd1f3b9d758ceecbd4
chinmaymudholkar1 <135991922+chinmaymudholkar1@users.noreply.github.com>
2024-09-20 09:38:58 +0100

    Create test_0_smoke.py

---
+++ tests/test_0_smoke.py
@@ -0,0 +1,33 @@
+import pytest
+from libs import CurrentExecution
+from libs import file_ops
+from playwright.sync_api import expect
+
+
+class Test_Smoke:
+    ce = CurrentExecution()
+    fo = file_ops.file_operations()
+
+    @pytest.fixture
+    def setup(self):
+        self.ce.start_execution()
+        yield
+        self.ce.end_execution()
+
+    @pytest.mark.smoke
+    @pytest.mark.order(1)
+    def test_smoke_files_and_paths(self):
+        assert self.fo.check_if_path_exists(file_or_folder_path=".env"), ".env file not found at the project root."
+        assert self.fo.check_if_path_exists(
+            file_or_folder_path="screenshots/"
+        ), "Screenshots folder does not exist at project root"
+
+    @pytest.mark.smoke
+    @pytest.mark.order(2)
+    def test_smoke_verify_packages(self):
+        pass
+
+    @pytest.mark.smoke
+    @pytest.mark.order(3)
+    def test_smoke_homepage_loads(self, setup):
+        expect(self.ce.page.locator("h1")).to_contain_text("Manage vaccinations in schools")
