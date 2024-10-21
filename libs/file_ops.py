d978b94ee0588340f9fbacd4a7851a8b0d2625be
chinmaymudholkar1 <135991922+chinmaymudholkar1@users.noreply.github.com>
2024-10-21 17:40:33 +0100

    Update file_ops.py

--- libs/file_ops.py
+++ libs/file_ops.py
@@ -6,7 +6,11 @@ from libs.constants import file_mode
 
 
 class file_operations:
-
+    def create_dir(self, dir_path: str) -> bool:
+        if not self.check_if_path_exists(file_or_folder_path=dir_path):
+            os.makedirs(name=dir_path)
+        return self.check_if_path_exists(file_or_folder_path=dir_path)
+        
     def check_if_path_exists(self, file_or_folder_path: str) -> bool:
         return True if path.exists(path=file_or_folder_path) else False
 
