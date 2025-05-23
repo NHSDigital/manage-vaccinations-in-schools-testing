9502068b5e326562eab633be0e7338cabd92e46f
Thomas Leese <thomas.leese1@nhs.net>
2025-05-23 11:57:26 +0100

    Flatten test_05_programmes

--- tests/test_05_programmes.py
+++ tests/test_05_programmes.py
@@ -16,226 +16,220 @@ from pages import (
 )
 
 
-class Test_Programmes:
-    login_page = LoginPage()
-    dashboard_page = DashboardPage()
-    sessions_page = SessionsPage()
-    programmes_page = ProgrammesPage()
-    import_records_page = ImportRecordsPage()
-    vaccines_page = VaccinesPage()
+login_page = LoginPage()
+dashboard_page = DashboardPage()
+sessions_page = SessionsPage()
+programmes_page = ProgrammesPage()
+import_records_page = ImportRecordsPage()
+vaccines_page = VaccinesPage()
 
-    @pytest.fixture(scope="function", autouse=False)
-    def setup_cohort_upload_and_reports(self, start_mavis, nurse):
-        self.login_page.log_in(**nurse)
-        self.dashboard_page.click_programmes()
+
+@pytest.fixture(scope="function", autouse=False)
+def setup_cohort_upload_and_reports(start_mavis, nurse):
+    login_page.log_in(**nurse)
+    dashboard_page.click_programmes()
+    yield
+    login_page.log_out()
+
+
+@pytest.fixture(scope="function", autouse=False)
+def setup_record_a_vaccine(start_mavis, nurse):
+    try:
+        login_page.log_in(**nurse)
+        dashboard_page.click_sessions()
+        sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
         yield
-        self.login_page.log_out()
+    finally:
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.delete_all_sessions_for_school_1()
+        login_page.log_out()
 
-    @pytest.fixture(scope="function", autouse=False)
-    def setup_record_a_vaccine(self, start_mavis, nurse):
-        try:
-            self.login_page.log_in(**nurse)
-            self.dashboard_page.click_sessions()
-            self.sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            yield
-        finally:
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.delete_all_sessions_for_school_1()
-            self.login_page.log_out()
 
-    @pytest.fixture(scope="function", autouse=False)
-    def setup_mavis_1729(self, start_mavis, nurse):
-        try:
-            self.login_page.log_in(**nurse)
-            self.dashboard_page.click_sessions()
-            self.sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
-            self.import_records_page.import_class_list_records_from_school_session(
-                file_paths=test_data_file_paths.CLASS_SESSION_ID
-            )
-            self.sessions_page.click_school1()
-            self.sessions_page.save_session_id_from_offline_excel()
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_import_records()
-            self.import_records_page.import_vaccination_records(
-                file_paths=test_data_file_paths.VACCS_HPV_DOSE_TWO,
-                file_type=mavis_file_types.VACCS_MAVIS,
-            )
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_programmes()
-            yield
-        finally:
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.delete_all_sessions_for_school_1()
-            self.login_page.log_out()
-
-    @pytest.fixture(scope="function", autouse=False)
-    def setup_mav_854(self, start_mavis, nurse):
-        try:
-            self.login_page.log_in(**nurse)
-            self.dashboard_page.click_vaccines()
-            self.vaccines_page.add_batch(vaccine_name=vaccines.GARDASIL9)
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
-            self.import_records_page.import_class_list_records_from_school_session(
-                file_paths=test_data_file_paths.CLASS_MAV_854
-            )
-            self.sessions_page.click_school1()
-            self.sessions_page.save_session_id_from_offline_excel()
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.schedule_a_valid_session_in_community_clinics(
-                for_today=True
-            )
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_children()
-            yield
-        finally:
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.delete_all_sessions_for_school_1()
-            self.login_page.log_out()
-
-    @pytest.fixture(scope="function", autouse=False)
-    def setup_mav_nnn(self, start_mavis, admin):
-        try:
-            self.login_page.log_in(**admin)
-            self.dashboard_page.click_sessions()
-            self.sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
-            self.import_records_page.import_class_list_records_from_school_session(
-                file_paths=test_data_file_paths.CLASS_SINGLE_VACC
-            )
-            self.sessions_page.click_school1()
-            self.sessions_page.save_session_id_from_offline_excel()
-            yield
-        finally:
-            self.dashboard_page.go_to_dashboard()
-            self.dashboard_page.click_sessions()
-            self.sessions_page.delete_all_sessions_for_school_1()
-            self.login_page.log_out()
-
-    @pytest.mark.cohorts
-    @pytest.mark.order(501)
-    def test_cohort_upload_positive(self, setup_cohort_upload_and_reports):
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_POSITIVE
+@pytest.fixture(scope="function", autouse=False)
+def setup_mavis_1729(start_mavis, nurse):
+    try:
+        login_page.log_in(**nurse)
+        dashboard_page.click_sessions()
+        sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
+        import_records_page.import_class_list_records_from_school_session(
+            file_paths=test_data_file_paths.CLASS_SESSION_ID
         )
-
-    @pytest.mark.cohorts
-    @pytest.mark.order(502)
-    def test_cohort_upload_negative(self, setup_cohort_upload_and_reports):
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_NEGATIVE
+        sessions_page.click_school1()
+        sessions_page.save_session_id_from_offline_excel()
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_import_records()
+        import_records_page.import_vaccination_records(
+            file_paths=test_data_file_paths.VACCS_HPV_DOSE_TWO,
+            file_type=mavis_file_types.VACCS_MAVIS,
         )
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_programmes()
+        yield
+    finally:
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.delete_all_sessions_for_school_1()
+        login_page.log_out()
 
-    @pytest.mark.cohorts
-    @pytest.mark.order(503)
-    def test_cohorts_file_structure(self, setup_cohort_upload_and_reports):
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_INVALID_STRUCTURE
+
+@pytest.fixture(scope="function", autouse=False)
+def setup_mav_854(start_mavis, nurse):
+    try:
+        login_page.log_in(**nurse)
+        dashboard_page.click_vaccines()
+        vaccines_page.add_batch(vaccine_name=vaccines.GARDASIL9)
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
+        import_records_page.import_class_list_records_from_school_session(
+            file_paths=test_data_file_paths.CLASS_MAV_854
         )
+        sessions_page.click_school1()
+        sessions_page.save_session_id_from_offline_excel()
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.schedule_a_valid_session_in_community_clinics(for_today=True)
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_children()
+        yield
+    finally:
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.delete_all_sessions_for_school_1()
+        login_page.log_out()
 
-    @pytest.mark.cohorts
-    @pytest.mark.order(504)
-    def test_cohorts_no_record(self, setup_cohort_upload_and_reports):
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_HEADER_ONLY
+
+@pytest.fixture(scope="function", autouse=False)
+def setup_mav_nnn(start_mavis, admin):
+    try:
+        login_page.log_in(**admin)
+        dashboard_page.click_sessions()
+        sessions_page.schedule_a_valid_session_in_school_1(for_today=True)
+        import_records_page.import_class_list_records_from_school_session(
+            file_paths=test_data_file_paths.CLASS_SINGLE_VACC
         )
+        sessions_page.click_school1()
+        sessions_page.save_session_id_from_offline_excel()
+        yield
+    finally:
+        dashboard_page.go_to_dashboard()
+        dashboard_page.click_sessions()
+        sessions_page.delete_all_sessions_for_school_1()
+        login_page.log_out()
 
-    @pytest.mark.cohorts
-    @pytest.mark.order(505)
-    def test_cohorts_empty_file(self, setup_cohort_upload_and_reports):
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_EMPTY_FILE
-        )
 
-    @pytest.mark.cohorts
-    @pytest.mark.bug
-    @pytest.mark.order(506)
-    def test_cohorts_readd_to_cohort(self, setup_cohort_upload_and_reports):  # MAV-909
-        self.programmes_page.upload_cohorts(
-            file_paths=test_data_file_paths.COHORTS_MAV_909
-        )
-        self.programmes_page.verify_mav_909()
+@pytest.mark.cohorts
+@pytest.mark.order(501)
+def test_cohort_upload_positive(setup_cohort_upload_and_reports):
+    programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_POSITIVE)
 
-    @pytest.mark.rav
-    @pytest.mark.order(526)
-    def test_programmes_rav_triage_positive(self, setup_record_a_vaccine):
-        self.sessions_page.update_triage_outcome_positive(
-            file_paths=test_data_file_paths.COHORTS_FULL_NAME
-        )
 
-    @pytest.mark.rav
-    @pytest.mark.order(527)
-    def test_programmes_rav_triage_consent_refused(self, setup_record_a_vaccine):
-        self.sessions_page.update_triage_outcome_consent_refused(
-            file_paths=test_data_file_paths.COHORTS_FULL_NAME
-        )
+@pytest.mark.cohorts
+@pytest.mark.order(502)
+def test_cohort_upload_negative(setup_cohort_upload_and_reports):
+    programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_NEGATIVE)
 
-    @pytest.mark.rav
-    @pytest.mark.bug
-    @pytest.mark.order(528)
-    def test_programmes_rav_edit_dose_to_not_given(self, setup_mavis_1729):
-        self.programmes_page.edit_dose_to_not_given()  # MAVIS-1729
 
-    @pytest.mark.rav
-    @pytest.mark.bug
-    @pytest.mark.order(529)
-    def test_programmes_rav_verify_excel_mav_854(self, setup_mav_854):
-        self.programmes_page.verify_mav_854()  # MAV-854
+@pytest.mark.cohorts
+@pytest.mark.order(503)
+def test_cohorts_file_structure(setup_cohort_upload_and_reports):
+    programmes_page.upload_cohorts(
+        file_paths=test_data_file_paths.COHORTS_INVALID_STRUCTURE
+    )
 
-    @pytest.mark.rav
-    @pytest.mark.order(530)
-    @pytest.mark.skip(reason="Test under construction")
-    def test_programmes_rav_verify_banners(self, setup_mav_nnn):
-        # self.programmes_page.verify_mav_nnn()
-        pass
 
-    @pytest.mark.reports
-    @pytest.mark.order(551)
-    def test_programmes_verify_careplus_report_for_hpv(
-        self, setup_cohort_upload_and_reports
-    ):
-        self.programmes_page.verify_careplus_report_format(for_programme=programmes.HPV)
+@pytest.mark.cohorts
+@pytest.mark.order(504)
+def test_cohorts_no_record(setup_cohort_upload_and_reports):
+    programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_HEADER_ONLY)
 
-    @pytest.mark.reports
-    @pytest.mark.order(552)
-    def test_programmes_verify_careplus_report_for_doubles(
-        self, setup_cohort_upload_and_reports
-    ):
-        self.programmes_page.verify_careplus_report_format(
-            for_programme=programmes.MENACWY
-        )
-        self.dashboard_page.go_to_dashboard()
-        self.dashboard_page.click_programmes()
-        self.programmes_page.verify_careplus_report_format(
-            for_programme=programmes.TDIPV
-        )
 
-    @pytest.mark.reports
-    @pytest.mark.order(553)
-    def test_programmes_verify_csv_report_for_hpv(
-        self, setup_cohort_upload_and_reports
-    ):
-        self.programmes_page.verify_csv_report_format(for_programme=programmes.HPV)
+@pytest.mark.cohorts
+@pytest.mark.order(505)
+def test_cohorts_empty_file(setup_cohort_upload_and_reports):
+    programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_EMPTY_FILE)
 
-    @pytest.mark.reports
-    @pytest.mark.order(554)
-    def test_programmes_verify_csv_report_for_doubles(
-        self, setup_cohort_upload_and_reports
-    ):
-        self.programmes_page.verify_csv_report_format(for_programme=programmes.MENACWY)
-        self.dashboard_page.go_to_dashboard()
-        self.dashboard_page.click_programmes()
-        self.programmes_page.verify_csv_report_format(for_programme=programmes.TDIPV)
 
-    @pytest.mark.reports
-    @pytest.mark.order(555)
-    def test_programmes_verify_systmone_report_for_hpv(
-        self, setup_cohort_upload_and_reports
-    ):
-        self.programmes_page.verify_systmone_report_format(for_programme=programmes.HPV)
+@pytest.mark.cohorts
+@pytest.mark.bug
+@pytest.mark.order(506)
+def test_cohorts_readd_to_cohort(setup_cohort_upload_and_reports):  # MAV-909
+    programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_MAV_909)
+    programmes_page.verify_mav_909()
+
+
+@pytest.mark.rav
+@pytest.mark.order(526)
+def test_rav_triage_positive(setup_record_a_vaccine):
+    sessions_page.update_triage_outcome_positive(
+        file_paths=test_data_file_paths.COHORTS_FULL_NAME
+    )
+
+
+@pytest.mark.rav
+@pytest.mark.order(527)
+def test_rav_triage_consent_refused(setup_record_a_vaccine):
+    sessions_page.update_triage_outcome_consent_refused(
+        file_paths=test_data_file_paths.COHORTS_FULL_NAME
+    )
+
+
+@pytest.mark.rav
+@pytest.mark.bug
+@pytest.mark.order(528)
+def test_rav_edit_dose_to_not_given(setup_mavis_1729):
+    programmes_page.edit_dose_to_not_given()  # MAVIS-1729
+
+
+@pytest.mark.rav
+@pytest.mark.bug
+@pytest.mark.order(529)
+def test_rav_verify_excel_mav_854(setup_mav_854):
+    programmes_page.verify_mav_854()  # MAV-854
+
+
+@pytest.mark.rav
+@pytest.mark.order(530)
+@pytest.mark.skip(reason="Test under construction")
+def test_rav_verify_banners(setup_mav_nnn):
+    # programmes_page.verify_mav_nnn()
+    pass
+
+
+@pytest.mark.reports
+@pytest.mark.order(551)
+def test_verify_careplus_report_for_hpv(setup_cohort_upload_and_reports):
+    programmes_page.verify_careplus_report_format(for_programme=programmes.HPV)
+
+
+@pytest.mark.reports
+@pytest.mark.order(552)
+def test_verify_careplus_report_for_doubles(setup_cohort_upload_and_reports):
+    programmes_page.verify_careplus_report_format(for_programme=programmes.MENACWY)
+    dashboard_page.go_to_dashboard()
+    dashboard_page.click_programmes()
+    programmes_page.verify_careplus_report_format(for_programme=programmes.TDIPV)
+
+
+@pytest.mark.reports
+@pytest.mark.order(553)
+def test_verify_csv_report_for_hpv(setup_cohort_upload_and_reports):
+    programmes_page.verify_csv_report_format(for_programme=programmes.HPV)
+
+
+@pytest.mark.reports
+@pytest.mark.order(554)
+def test_verify_csv_report_for_doubles(setup_cohort_upload_and_reports):
+    programmes_page.verify_csv_report_format(for_programme=programmes.MENACWY)
+    dashboard_page.go_to_dashboard()
+    dashboard_page.click_programmes()
+    programmes_page.verify_csv_report_format(for_programme=programmes.TDIPV)
+
+
+@pytest.mark.reports
+@pytest.mark.order(555)
+def test_verify_systmone_report_for_hpv(setup_cohort_upload_and_reports):
+    programmes_page.verify_systmone_report_format(for_programme=programmes.HPV)
