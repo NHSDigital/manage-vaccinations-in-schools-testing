ff4c68efbeb0de528b506868bbdd61b14b2818a5
Joshua Frost <joshuafrost.work@gmail.com>
2025-05-29 15:04:51 +0000

    migrate programmes page to pom

--- mavis/test/models/school_moves.py
+++ mavis/test/models/school_moves.py
@@ -3,8 +3,9 @@ from typing import Final, List
 import pandas as pd
 
 from ..generic_constants import actions, escape_characters, properties
-from ..mavis_constants import report_headers
+
 from ..onboarding import School
+from ..mavis_constants import ReportFormat
 from ..playwright_ops import PlaywrightOperations
 from ..wrappers import get_current_datetime
 
@@ -110,7 +111,7 @@ class SchoolMovesPage:
         self.po.act(locator=self.LNK_DOWNLOAD_RECORDS, action=actions.CLICK_BUTTON)
         self.po.act(locator=self.BTN_CONTINUE, action=actions.CLICK_BUTTON)
         self._download_and_verify_report_headers(
-            expected_headers=report_headers.SCHOOL_MOVES
+            expected_headers=ReportFormat.SCHOOL_MOVES.headers
         )
 
     def _download_and_verify_report_headers(self, expected_headers: str):
--- tests/test_programmes.py
+++ tests/test_programmes.py
@@ -6,6 +6,7 @@ from mavis.test.mavis_constants import (
     test_data_file_paths,
     Programme,
     Vaccine,
+    ReportFormat
 )
 
 
@@ -174,36 +175,36 @@ def test_rav_verify_banners(setup_mav_nnn):
 def test_verify_careplus_report_for_hpv(
     setup_cohort_upload_and_reports, programmes_page
 ):
-    programmes_page.verify_careplus_report_format(Programme.HPV)
+    programmes_page.verify_report_format(programme=Programme.HPV, report_format=ReportFormat.CAREPLUS)
 
 
 @pytest.mark.reports
 def test_verify_careplus_report_for_doubles(
     setup_cohort_upload_and_reports, dashboard_page, programmes_page
 ):
-    programmes_page.verify_careplus_report_format(Programme.MENACWY)
+    programmes_page.verify_report_format(programme=Programme.MENACWY, report_format=ReportFormat.CAREPLUS)
     dashboard_page.click_mavis()
     dashboard_page.click_programmes()
-    programmes_page.verify_careplus_report_format(Programme.TD_IPV)
+    programmes_page.verify_report_format(programme=Programme.TD_IPV, report_format=ReportFormat.CAREPLUS)
 
 
 @pytest.mark.reports
 def test_verify_csv_report_for_hpv(setup_cohort_upload_and_reports, programmes_page):
-    programmes_page.verify_csv_report_format(Programme.HPV)
+    programmes_page.verify_report_format(programme=Programme.HPV, report_format=ReportFormat.CSV)
 
 
 @pytest.mark.reports
 def test_verify_csv_report_for_doubles(
     setup_cohort_upload_and_reports, dashboard_page, programmes_page
 ):
-    programmes_page.verify_csv_report_format(Programme.MENACWY)
+    programmes_page.verify_report_format(programme=Programme.MENACWY, report_format=ReportFormat.CSV)
     dashboard_page.click_mavis()
     dashboard_page.click_programmes()
-    programmes_page.verify_csv_report_format(Programme.TD_IPV)
+    programmes_page.verify_report_format(programme=Programme.TD_IPV, report_format=ReportFormat.CSV)
 
 
 @pytest.mark.reports
 def test_verify_systmone_report_for_hpv(
     setup_cohort_upload_and_reports, programmes_page
 ):
-    programmes_page.verify_systmone_report_format(Programme.HPV)
+    programmes_page.verify_report_format(programme=Programme.HPV, report_format=ReportFormat.SYSTMONE)
