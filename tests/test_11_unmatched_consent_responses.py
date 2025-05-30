1da1fe4a5edac36e643fd17f22c09b666f67f38d
Thomas Leese <thomas.leese1@nhs.net>
2025-05-30 07:37:33 +0100

    Refactor unmatched consent responses tests
    This refactors the unmatched consent responses tests to no longer depend
    on the online consent tests for HPV and Doubles to set up the scenario,
    meaning they can be run independently.

--- tests/test_11_unmatched_consent_responses.py
+++ tests/test_11_unmatched_consent_responses.py
@@ -1,58 +1,99 @@
+import allure
 import pytest
 
-from mavis.test.mavis_constants import test_data_file_paths
+from mavis.test.mavis_constants import test_data_file_paths, Programme
 
-
-# ALL OF THE TESTS IN THIS CLASS DEPEND ON THE CONSENT WORKFLOW TESTS (HPV) TO HAVE RUN FIRST
-# RUN THE CONSENT WORKFLOW TESTS OR THE FULL PACK BEFORE RUNNING THESE TESTS
-# RUN WITH '--skip-reset' IF RUNNING ONLY CONSENT TESTS
+pytestmark = pytest.mark.unmatched_consent_responses
 
 
 @pytest.fixture
+def online_consent_url(get_online_consent_url):
+    yield from get_online_consent_url(Programme.HPV)
+
+
+@pytest.fixture
+def child_name(faker):
+    return faker.first_name(), faker.last_name()
+
+
+@pytest.fixture
+def child_date_of_birth():
+    return 12, 8, 2009
+
+
+@pytest.fixture
+def child_address():
+    return (
+        "1 ROWSLEY AVENUE",
+        "",
+        "DERBY",
+        "DE23 6JZ",
+    )
+
+
+@pytest.fixture(autouse=True)
+def give_online_consent(
+    page,
+    start_page,
+    consent_page,
+    online_consent_url,
+    child_name,
+    child_date_of_birth,
+    child_address,
+    schools,
+    faker,
+):
+    page.goto(online_consent_url)
+    start_page.start()
+    consent_page.fill_child_name_details(*child_name)
+    consent_page.fill_child_dob(*child_date_of_birth)
+    consent_page.select_child_school(schools[0])
+    consent_page.fill_parent_details("Parent Full", "Dad", email=faker.email())
+    consent_page.select_consent_for_hpv_vaccination(True)
+    consent_page.fill_address_details(*child_address)
+    for _ in range(4):
+        consent_page.select_and_provide_details(None)
+    consent_page.click_confirm_details()
+
+
+@pytest.fixture(autouse=True)
 def go_to_unmatched_consent_responses(log_in_as_nurse, dashboard_page):
     dashboard_page.click_unmatched_consent_responses()
 
 
-@pytest.fixture
-def setup_ucr_match(log_in_as_nurse, dashboard_page, programmes_page):
+@pytest.mark.order(1002)
+@allure.issue("MAVIS-1782")
+def test_archive_record(unmatched_page, child_name):
+    unmatched_page.archive_record(*child_name)
+
+
+@pytest.mark.order(1004)
+@allure.issue("MAVIS-1812")
+@pytest.mark.parametrize("child_name", [("CMatch1", "CMatch1")])
+def test_match_record(
+    dashboard_page, programmes_page, unmatched_page, schools, child_name
+):
+    dashboard_page.click_mavis()
     dashboard_page.click_programmes()
     programmes_page.upload_cohorts(file_paths=test_data_file_paths.COHORTS_UCR_MATCH)
     dashboard_page.click_mavis()
     dashboard_page.click_unmatched_consent_responses()
 
-
-@pytest.mark.unmatchedconsentresponses
-@pytest.mark.order(1001)
-@pytest.mark.dependency(name="ucr_records_exist")
-def test_check_records_exist(go_to_unmatched_consent_responses, unmatched_page):
-    unmatched_page.verify_records_exist()
+    unmatched_page.match_with_record(schools[0], "CMatch1", "CMatch1")
 
 
-@pytest.mark.unmatchedconsentresponses
-@pytest.mark.order(1002)
-@pytest.mark.dependency(depends=["ucr_records_exist"])
-def test_archive_record(go_to_unmatched_consent_responses, unmatched_page):
-    unmatched_page.archive_record()  # Covers MAVIS-1782
-
-
-@pytest.mark.unmatchedconsentresponses
 @pytest.mark.order(1003)
-@pytest.mark.dependency(depends=["ucr_records_exist"])
-def test_create_record(go_to_unmatched_consent_responses, schools, unmatched_page):
-    unmatched_page.create_record(schools[0])  # Covers MAVIS-1812
+@allure.issue("MAVIS-1812")
+@pytest.mark.parametrize("child_name", [("Helena", "Hoyte")])
+@pytest.mark.parametrize("child_date_of_birth", [(20, 8, 2011)])
+@pytest.mark.parametrize(
+    "child_address", [("1 WEST PARK PLACE", "RETFORD", "NOTTS", "DN22 7PP")]
+)
+def test_create_record_with_nhs_number(unmatched_page, schools, child_name):
+    unmatched_page.create_record(schools[0], *child_name)
 
 
-@pytest.mark.unmatchedconsentresponses
-@pytest.mark.order(1004)
-@pytest.mark.dependency(depends=["ucr_records_exist"])
-def test_match_record(setup_ucr_match, schools, unmatched_page):
-    unmatched_page.match_with_record(schools[0])  # Covers MAVIS-1812
-
-
-@pytest.mark.unmatchedconsentresponses
 @pytest.mark.order(1005)
-@pytest.mark.dependency(depends=["ucr_records_exist"])
-def test_create_record_with_no_nhs_number(
-    go_to_unmatched_consent_responses, schools, unmatched_page
-):
-    unmatched_page.create_record_with_no_nhs_number(schools[0])  # MAVIS-1781
+@allure.issue("MAVIS-1781")
+def test_create_record_with_no_nhs_number(unmatched_page, schools, child_name):
+    unmatched_page.create_record_with_no_nhs_number(schools[0], *child_name)
