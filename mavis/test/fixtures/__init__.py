fc67b182462c7248bf0f90e3ba61ffe8d74d0059
Thomas Leese <thomas.leese1@nhs.net>
2025-05-31 11:48:31 +0100

    Move playwright fixtures in to `mavis.test`
    This moves the fixtures related to setting up Playwright in to their own
    file.

---
+++ mavis/test/fixtures/__init__.py
@@ -0,0 +1,15 @@
+from .playwright import (
+    base_url,
+    basic_auth,
+    browser_context_args,
+    playwright_operations,
+    screenshots_path,
+)
+
+__all__ = [
+    "base_url",
+    "basic_auth",
+    "browser_context_args",
+    "playwright_operations",
+    "screenshots_path",
+]
