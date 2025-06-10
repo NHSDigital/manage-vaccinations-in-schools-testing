a7bb6dbfb63cb9a8e1dff15d1908c0795ceb067c
Joshua Frost <joshuafrost.work@gmail.com>
2025-06-10 10:02:58 +0100

    filter out patients in Python

--- mavis/test/data/pds.py
+++ mavis/test/data/pds.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 import csv
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 class Patient(NamedTuple):
@@ -13,12 +13,14 @@ class Patient(NamedTuple):
     address_line_2: str
     address_town: str
     address_postcode: str
+    date_of_death: Optional[date] = None
 
     @classmethod
     def from_csv_row(cls, row):
         address_parts = [
             row[f"ADDRESS_LINE_{i}"] for i in range(1, 5) if row[f"ADDRESS_LINE_{i}"]
         ]
+        date_of_death_string = row.get("DATE_OF_DEATH", "")[:8]
 
         return cls(
             nhs_number=row["NHS_NUMBER"],
@@ -29,6 +31,11 @@ class Patient(NamedTuple):
             address_line_2=address_parts[1],
             address_town=row["ADDRESS_LINE_4"],
             address_postcode=row["POST_CODE"],
+            date_of_death=(
+                datetime.strptime(date_of_death_string, "%Y%m%d").date()
+                if date_of_death_string
+                else None
+            ),
         )
 
     @property
@@ -45,6 +52,16 @@ class Patient(NamedTuple):
         )
 
 
-with (Path(__file__).parent / "pds_children.csv").open(newline="") as file:
+with (Path(__file__).parent / "pds.csv").open(newline="") as file:
     reader = csv.DictReader(file)
     patients = [Patient.from_csv_row(row) for row in reader]
+
+patients_without_date_of_death = [
+    patient for patient in patients if not patient.date_of_death
+]
+
+child_patients_without_date_of_death = [
+    patient
+    for patient in patients_without_date_of_death
+    if patient.date_of_birth >= date(2003, 6, 10)
+]
