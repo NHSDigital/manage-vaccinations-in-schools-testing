a760f6685af567d764f1d081d9e5d7a419b30578
Thomas Leese <thomas.leese1@nhs.net>
2025-06-12 08:18:34 +0100

    Refactor vaccine pages
    This refactors the vaccine page object in to separate page objects for
    each distinct page in the service to ensure the classes and smaller and
    don't leak elements from other pages.
    
    I've also added a test to ensure the batch names are at least 2
    characters long.
    
    This change depends on a few changes to the service being deployed
    first, specifically the improved accessibility labels:
    https://github.com/nhsuk/manage-vaccinations-in-schools/pull/3717

--- mavis/test/pages/vaccines.py
+++ mavis/test/pages/vaccines.py
@@ -1,95 +1,95 @@
-from playwright.sync_api import Page, expect
+from datetime import date
+
+from playwright.sync_api import Page
 
 from ..models import Vaccine
 from ..step import step
-from ..wrappers import get_current_datetime, get_offset_date
+
+
+class BatchExpiryDateMixin:
+    def __init__(self, page: Page):
+        self.expiry_day_textbox = page.get_by_role("textbox", name="Day")
+        self.expiry_month_textbox = page.get_by_role("textbox", name="Month")
+        self.expiry_year_textbox = page.get_by_role("textbox", name="Year")
+
+    @step("Fill in expiry date with {1}")
+    def fill_expiry_date(self, value: date):
+        self.expiry_day_textbox.fill(str(value.day))
+        self.expiry_month_textbox.fill(str(value.month))
+        self.expiry_year_textbox.fill(str(value.year))
+
+
+class AddBatchPage(BatchExpiryDateMixin):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.page = page
+
+        self.name_textbox = page.get_by_role("textbox", name="Batch")
+        self.confirm_button = page.get_by_role("button", name="Add batch")
+        self.success_alert = page.get_by_role("alert", name="Success").filter(
+            has_text="added"
+        )
+
+        error_alert = page.get_by_role("alert").filter(has_text="There is a problem")
+        self.error_listitem = error_alert.get_by_role("listitem")
+
+    @step("Fill in name with {1}")
+    def fill_name(self, value: str):
+        self.name_textbox.fill(value)
+
+    @step("Confirm add batch")
+    def confirm(self):
+        self.confirm_button.click()
+
+
+class EditBatchPage(BatchExpiryDateMixin):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.page = page
+
+        self.confirm_button = page.get_by_role("button", name="Save changes")
+        self.success_alert = page.get_by_role("alert", name="Success").filter(
+            has_text="updated"
+        )
+
+    @step("Confirm edit batch")
+    def confirm(self):
+        self.confirm_button.click()
+
+
+class ArchiveBatchPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.confirm_button = page.get_by_role("button", name="Yes, archive this batch")
+        self.success_alert = page.get_by_role("alert", name="Success").filter(
+            has_text="archived"
+        )
+
+    @step("Click on Archive this batch")
+    def confirm(self):
+        self.confirm_button.click()
 
 
 class VaccinesPage:
     def __init__(self, page: Page):
         self.page = page
 
-        self.batch_textbox = self.page.get_by_role("textbox", name="Batch")
-        self.day_textbox = self.page.get_by_role("textbox", name="Day")
-        self.month_textbox = self.page.get_by_role("textbox", name="Month")
-        self.year_textbox = self.page.get_by_role("textbox", name="Year")
-        self.add_batch_button = self.page.get_by_role("button", name="Add batch")
-        self.save_changes_button = self.page.get_by_role("button", name="Save changes")
-        self.confirm_archive_button = self.page.get_by_role(
-            "button", name="Yes, archive this batch"
-        )
-        self.batch_added_alert = page.get_by_role("alert", name="Success").filter(
-            has_text="added"
-        )
-        self.batch_name_error = (
-            page.locator("div").filter(has_text="There is a problemEnter a").nth(3)
-        )
+    @step("Go to vaccines page")
+    def navigate(self):
+        self.page.goto("/vaccines")
 
-    def _calculate_batch_details(self, vaccine: Vaccine, batch_name: str):
-        self.batch_name = (
-            f"{vaccine.replace(' ', '').replace('-', '')}{get_current_datetime()}"
-            if batch_name == ""
-            else batch_name
-        )
-        self.future_expiry_date = get_offset_date(offset_days=365)
-        self.day = self.future_expiry_date[-2:]
-        self.month = self.future_expiry_date[4:6]
-        self.year = self.future_expiry_date[:4]
-
-    @step("Add a new batch for {1}")
-    def add_batch(self, vaccine: Vaccine, batch_name: str = "") -> str:
-        self._calculate_batch_details(vaccine, batch_name=batch_name)
-        expect(self.page.get_by_role("main")).to_contain_text(vaccine)
-
+    @step("Add batch for {1}")
+    def click_add_batch(self, vaccine: Vaccine):
         self.page.get_by_role("link", name=f"Add a new {vaccine} batch").click()
 
-        expect(self.page.get_by_role("main")).to_contain_text(vaccine)
-        self.fill_batch_details()
-        self.click_add_batch_button()
-        if len(batch_name) <= 100:
-            expect(self.batch_added_alert).to_be_visible()
-        else:
-            expect(self.batch_name_error).to_be_visible()
+    @step("Change {2} batch for {1}")
+    def click_change_batch(self, vaccine: Vaccine, batch_name: str):
+        name = f"Change {batch_name} batch of {vaccine}"
+        self.page.get_by_role("link", name=name).click()
 
-        return self.batch_name
-
-    @step("Fill the batch details")
-    def fill_batch_details(self):
-        self.batch_textbox.fill(value=self.batch_name)
-        self.day_textbox.fill(value=self.day)
-        self.month_textbox.fill(value=self.month)
-        self.year_textbox.fill(value=self.year)
-
-    @step("Click on Add batch")
-    def click_add_batch_button(self):
-        self.add_batch_button.click()
-
-    @step("Click on Save changes")
-    def click_save_changes(self):
-        self.save_changes_button.click()
-
-    @step("Click on Archive this batch")
-    def click_archive_this_batch(self):
-        self.confirm_archive_button.click()
-
-    @step("Change the batch for {1}")
-    def change_batch(self, vaccine: Vaccine):
-        batch_name = self.batch_name or str(vaccine)
-        self.__click_batch_option(batch_name, "Change")
-        self.year_textbox.fill(value=get_offset_date(offset_days=730)[:4])
-        self.click_save_changes()
-        _success_message = f"Batch {self.batch_name} updated"
-        expect(self.page.get_by_role("alert")).to_contain_text(_success_message)
-
-    @step("Archive the batch for {1}")
-    def archive_batch(self, vaccine: Vaccine):
-        batch_name = self.batch_name or str(vaccine)
-        self.__click_batch_option(batch_name, "Archive")
-        self.click_archive_this_batch()
-        expect(self.page.get_by_role("alert")).to_contain_text("Batch archived.")
-
-    def __click_batch_option(self, batch_name: str, link_text: str):
-        row = self.page.locator("tr").filter(
-            has=self.page.locator("td:first-child", has_text=batch_name)
-        )
-        row.locator("td:last-child").get_by_role("link", name=link_text).click()
+    @step("Archive {2} batch for {1}")
+    def click_archive_batch(self, vaccine: Vaccine, batch_name: str):
+        name = f"Archive {batch_name} batch of {vaccine}"
+        self.page.get_by_role("link", name=name).click()
