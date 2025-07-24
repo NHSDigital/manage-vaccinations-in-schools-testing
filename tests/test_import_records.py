338effd48b74654efaf4b24d4ef5b7af9ae02d3a
Chinmay Mudholkar <chinmay.mudholkar1@nhs.net>
2025-07-24 16:24:18 +0100

    reviews

--- tests/test_import_records.py
+++ tests/test_import_records.py
@@ -1,5 +1,6 @@
 import pytest
 
+from mavis.test.annotations import issue
 from mavis.test.data import ChildFileMapping, ClassFileMapping, VaccsFileMapping
 from mavis.test.models import Programme
 
@@ -314,6 +315,7 @@ def test_vaccs_systmone_space_normalization(setup_vaccs_systmone, import_records
     )
 
 
+@issue("MAV-1547")
 @pytest.mark.vaccinations
 @pytest.mark.bug
 def test_vaccs_nivs_disallow_flu_for_previous_years(setup_vaccs, import_records_page):
@@ -322,6 +324,7 @@ def test_vaccs_nivs_disallow_flu_for_previous_years(setup_vaccs, import_records_
     )
 
 
+@issue("MAV-1599")
 @pytest.mark.vaccinations
 @pytest.mark.bug
 def test_vaccs_systmone_disallow_flu_for_previous_years(
