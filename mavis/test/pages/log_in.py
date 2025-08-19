d3d43f31b1409acbcde0a241fb03e55ec8bbe857
Joshua Frost <joshuafrost.work@gmail.com>
2025-08-19 16:50:15 +0100

    await start page after clicking logout

--- mavis/test/pages/log_in.py
+++ mavis/test/pages/log_in.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from mavis.test.models import User, Team
 from mavis.test.annotations import step
@@ -17,6 +17,7 @@ class LogInPage:
         self.log_out_button = page.get_by_role("button", name="Log out")
         self.continue_button = page.get_by_role("button", name="Continue")
         self.select_a_team_heading = page.get_by_text("Select a team")
+        self.start_page_link = page.get_by_role("link", name="Start now")
 
     @step("Go to log in page")
     def navigate(self):
@@ -31,6 +32,7 @@ class LogInPage:
     @step("Log out")
     def log_out(self):
         self.log_out_button.click()
+        expect(self.start_page_link).to_be_visible()
 
     @step("Log in as {1} and choose team {2}")
     def log_in_and_choose_team_if_necessary(self, user: User, team: Team):
