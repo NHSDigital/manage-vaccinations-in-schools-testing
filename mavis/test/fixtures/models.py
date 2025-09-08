607654a219e2931a28146405d3b1fdb80ec38297
Joshua Frost <joshuafrost.work@gmail.com>
2025-09-08 14:33:00 +0100

    add healthcare assistant role and update psd test

--- mavis/test/fixtures/models.py
+++ mavis/test/fixtures/models.py
@@ -59,6 +59,12 @@ def prescriber():
 
 
 @pytest.fixture(scope="session")
+def healthcare_assistant():
+    email = onboarding_faker.email()
+    return User(username=email, password=email, role="healthcare_assistant")
+
+
+@pytest.fixture(scope="session")
 def year_groups():
     return {
         programme.group: random.choice(programme.year_groups) for programme in Programme
@@ -160,12 +166,15 @@ def team(subteam, organisation) -> Team:
 
 
 @pytest.fixture(scope="session")
-def users(medical_secretary, nurse, superuser, prescriber) -> dict[str, User]:
+def users(
+    medical_secretary, nurse, superuser, prescriber, healthcare_assistant
+) -> dict[str, User]:
     return {
         "medical_secretary": medical_secretary,
         "nurse": nurse,
         "superuser": superuser,
         "prescriber": prescriber,
+        "healthcare_assistant": healthcare_assistant,
     }
 
 
