51692fe49017c1f929d5501e26888ec4529c2264
Joshua Frost <joshuafrost.work@gmail.com>
2025-12-08 16:55:29 +0000

    split imms_api fixtures

--- mavis/test/fixtures/data_models.py
+++ mavis/test/fixtures/data_models.py
@@ -1,17 +1,15 @@
-import base64
 import logging
 import os
 import random
 import time
 import urllib.parse
-import uuid
 
-import jwt
 import pytest
 import requests
 from faker import Faker
 
 from mavis.test.constants import Programme
+from mavis.test.data.file_generator import FileGenerator
 from mavis.test.data_models import (
     Child,
     Clinic,
@@ -28,6 +26,22 @@ logger = logging.getLogger(__name__)
 onboarding_faker = Faker(locale="en_GB")
 onboarding_faker.seed_instance(seed=time.time())
 onboarding_faker.unique.clear()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def delete_team_after_tests(base_url, team):
+    yield
+
+    url = urllib.parse.urljoin(base_url, f"api/testing/teams/{team.workgroup}")
+    response = requests.delete(url, timeout=30)
+    _check_response_status(response)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def reset_before_each_module(base_url, team) -> None:
+    url = urllib.parse.urljoin(base_url, f"api/testing/teams/{team.workgroup}")
+    response = requests.delete(url, params={"keep_itself": "true"}, timeout=30)
+    _check_response_status(response)
 
 
 @pytest.fixture(scope="session")
@@ -82,22 +96,6 @@ def _check_response_status(response) -> None:
     if not response.ok:
         logger.warning(response.content)
     response.raise_for_status()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def delete_team_after_tests(base_url, team):
-    yield
-
-    url = urllib.parse.urljoin(base_url, f"api/testing/teams/{team.workgroup}")
-    response = requests.delete(url, timeout=30)
-    _check_response_status(response)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def reset_before_each_module(base_url, team) -> None:
-    url = urllib.parse.urljoin(base_url, f"api/testing/teams/{team.workgroup}")
-    response = requests.delete(url, params={"keep_itself": "true"}, timeout=30)
-    _check_response_status(response)
 
 
 @pytest.fixture(scope="session")
@@ -157,57 +155,6 @@ def children(year_groups) -> dict[str, list[Child]]:
     )
 
 
-def _read_imms_api_credentials() -> dict[str, str]:
-    return {
-        "pem": os.environ["IMMS_API_PEM"],
-        "key": os.environ["IMMS_API_KEY"],
-        "kid": os.environ["IMMS_API_KID"],
-        "url": os.environ["IMMS_BASE_URL"],
-    }
-
-
-def _get_jwt_payload(api_auth: dict[str, str]) -> str:
-    _kid = api_auth["kid"]
-    _api_key = api_auth["key"]
-    _decoded_pem = base64.b64decode(api_auth["pem"])
-    _auth_endpoint = urllib.parse.urljoin(api_auth["url"], "oauth2-mock/token")
-    headers = {
-        "alg": "RS512",
-        "typ": "JWT",
-        "kid": _kid,
-    }
-    claims = {
-        "sub": _api_key,
-        "iss": _api_key,
-        "jti": str(uuid.uuid4()),
-        "aud": _auth_endpoint,
-        "exp": int(time.time()) + 300,  # 5mins in the future
-    }
-    return jwt.encode(
-        payload=claims,
-        key=_decoded_pem,
-        algorithm="RS512",
-        headers=headers,
-    )
-
-
-@pytest.fixture(scope="session", autouse=False)
-def authenticate_api():
-    _api_auth: dict[str, str] = _read_imms_api_credentials()
-    _endpoint = urllib.parse.urljoin(_api_auth["url"], "oauth2-mock/token")
-    _payload = {
-        "grant_type": "client_credentials",
-        "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",  # noqa: E501
-        "client_assertion": _get_jwt_payload(api_auth=_api_auth),
-    }
-    _headers = {"Content-Type": "application/x-www-form-urlencoded"}
-
-    response = requests.post(url=_endpoint, headers=_headers, data=_payload, timeout=30)
-
-    _check_response_status(response=response)
-    return response.json()["access_token"]
-
-
-@pytest.fixture(scope="session")
-def imms_base_url():
-    return os.environ["IMMS_AUTH_URL"]
+@pytest.fixture
+def file_generator(organisation, schools, nurse, children, clinics, year_groups):
+    return FileGenerator(organisation, schools, nurse, children, clinics, year_groups)
