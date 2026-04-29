e33ec4e146b02dec4ce3fcfc155699d6ee35e77b
Zoltan Antal <zoltan_antal@outlook.com>
2026-04-29 17:19:54 +0100

    Link child school to team

--- mavis/test/fixtures/create_child.py
+++ mavis/test/fixtures/create_child.py
@@ -4,7 +4,7 @@ import urllib.parse
 import httpx
 import pytest
 
-from mavis.test.data_models import Child, School
+from mavis.test.data_models import Child, School, Team
 
 
 def create_patient(base_url: str, child: Child, school_id: int) -> Child:
@@ -24,13 +24,28 @@ def create_patient(base_url: str, child: Child, school_id: int) -> Child:
     response.raise_for_status()
     return child
 
+
+def link_location_to_workgroup_teams(
+    base_url: str, workgroup: str, school_id: int
+) -> None:
+    url = urllib.parse.urljoin(base_url, "/api/testing/team-locations")
+    data = {"workgroup": workgroup, "school_id": school_id}
+    response = httpx.post(url, json=data)
+    response.raise_for_status()
+
+
 @pytest.fixture
 def create_child(base_url):
-    def _create_child(year_group: int) -> Child:
+    def _create_child(year_group: int, team: Team) -> Child:
         schools = School.get_schools_by_year_group(base_url, [year_group])
         selected_school = random.choice(schools)
         child = Child.generate(year_group=year_group)
         create_patient(base_url, child, selected_school.id)
+        link_location_to_workgroup_teams(
+            base_url,
+            workgroup=team.workgroup,
+            school_id=selected_school.id,
+        )
         return child
 
     return _create_child
